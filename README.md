f8356ece783d9a68918672c2c289c92cdf8cb713
Arthur Nunes <coletivosuburbano@gmail.com>
2021-10-19 19:01:26 -0300

    Feature: ReadMe + Chore: Mirage Pagination Example

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# nextjs-chakraui-boilerplate
+## Project Name
+
+## Figma
+
+1. `figma link`
+
+## GDrive
+
+1. `gdrive link`
+
+## Git Clone
+
+1.  `git clone -b devel ssh-here`.
+
+## Git Flow
+
+1.  `git checkout -B prefix/assunto`.
+2.  `git fetch origin devel`.
+3.  `git merge origin/devel`.
+
+## Prefix
+
+- **Feature**: Novas funções.
+- **Chore**: Melhoria/refatoração no código.
+- **Hotfix**: Para corrigir urgências em produção.
+- **Bug**: Resolução de bugs.
+
+## Subir
+
+1.  `git add .` _ou_ `git add nome_do_arquivo`.
+2.  `git commit -m "prefix: assunto"`.
+3.  `git push -u origin branch_criada` _(Após a primeira vez, apenas `git push`)_.
+
+## GitHub
+
+1.  Pull Request da **branch_criada** para **develop**. _(Sempre colocar comentário descrevendo solução e ligar à **issue**)_
+    _Exemplo: "Resolução Bug - Botão sair - Pull Request ligada à issue #23."_
+2.  **Sempre** solicitar review do código através das Tags.
+3.  Ao finalizar, **delete** sua branch localmente `git branch -d branch_criada`.
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,64 +1,64 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
-import faker from 'faker';
+// import { createServer, Factory, Model, Response } from 'miragejs';
+// import faker from 'faker';
 
-// Tipagem do objeto
-type User = {
-  name: string;
-  email: string;
-  created_at: string;
-};
+// // Tipagem do objeto
+// type User = {
+//   name: string;
+//   email: string;
+//   created_at: string;
+// };
 
-export function makeServer() {
-  const server = createServer({
-    models: {
-      user: Model.extend<Partial<User>>({}),
-    },
+// export function makeServer() {
+//   const server = createServer({
+//     models: {
+//       user: Model.extend<Partial<User>>({}),
+//     },
 
-    factories: {
-      user: Factory.extend({
-        name(i: number) {
-          return `User ${i + 1}`;
-        },
-        email() {
-          return faker.internet.email().toLocaleLowerCase(); // Gera um email aleatório
-        },
-        created_at() {
-          return faker.date.recent(10); // 10 dias atrás
-        },
-      }),
-    },
+//     factories: {
+//       user: Factory.extend({
+//         name(i: number) {
+//           return `User ${i + 1}`;
+//         },
+//         email() {
+//           return faker.internet.email().toLocaleLowerCase(); // Gera um email aleatório
+//         },
+//         created_at() {
+//           return faker.date.recent(10); // 10 dias atrás
+//         },
+//       }),
+//     },
 
-    seeds(server) {
-      server.createList('user', 10); // Cria 10 usuários
-    },
+//     seeds(server) {
+//       server.createList('user', 10); // Cria 10 usuários
+//     },
 
-    routes() {
-      this.namespace = 'api';
-      this.timing = 750;
+//     routes() {
+//       this.namespace = 'api';
+//       this.timing = 750;
 
-      this.get('users');
-      this.post('/users');
+//       this.get('users');
+//       this.post('/users');
 
-      // // Pagination Example
-      // this.get('/users', function (schema, request) {
-      //   const { page = 1, perPage = 10 } = request.queryParams;
-      //   const total = schema.all('user').length;
+//       // // Pagination Example
+//       // this.get('/users', function (schema, request) {
+//       //   const { page = 1, perPage = 10 } = request.queryParams;
+//       //   const total = schema.all('user').length;
 
-      //   const pageStart = (Number(page) - 1) * Number(perPage);
-      //   const pageEnd = pageStart + Number(perPage);
+//       //   const pageStart = (Number(page) - 1) * Number(perPage);
+//       //   const pageEnd = pageStart + Number(perPage);
 
-      //   const users = this.serialize(schema.all('user')).users.slice(
-      //     pageStart,
-      //     pageEnd
-      //   );
+//       //   const users = this.serialize(schema.all('user')).users.slice(
+//       //     pageStart,
+//       //     pageEnd
+//       //   );
 
-      //   return new Response(200, { 'x-total-count': String(total) }, { users });
-      // });
+//       //   return new Response(200, { 'x-total-count': String(total) }, { users });
+//       // });
 
-      this.namespace = '';
-      this.passthrough();
-    },
-  });
+//       this.namespace = '';
+//       this.passthrough();
+//     },
+//   });
 
-  return server;
-}
+//   return server;
+// }
